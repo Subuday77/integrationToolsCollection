43066001a0ee78024a79558615a369f7924032a6
Subuday77 <subuday.bagatur@gmail.com>
2020-10-26 15:41:02 +0200

    Initial commit

---
+++ src/main/java/com/integrationToolsCollection/rest/CollectionController.java
@@ -0,0 +1,103 @@
+package com.integrationToolsCollection.rest;
+
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+@CrossOrigin(origins = "*")
+
+public class CollectionController {
+
+
+    @GetMapping("/getfile")
+    public ResponseEntity<?> getFile(@RequestParam(name = "id") int id) throws IOException {
+        String fileName = "";
+        switch (id) {
+            case 1:
+                fileName = "_Stress_Test_Pattern v.1.1.0.xlsm";
+                File file = new File("files/" + fileName);
+                if (file.exists()) {
+                    Path path = Paths.get(file.getAbsolutePath());
+                    ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName);
+                    return ResponseEntity.ok().headers(headers).contentLength(file.length())
+                            .contentType(MediaType
+                                    .parseMediaType("application/x-msexcel"))
+                            .body(resource);
+                }
+                return new ResponseEntity<String>("File not found", HttpStatus.NOT_FOUND);
+
+            case 2:
+                fileName = "Log Parser Local Server v.1.1.0.exe";
+                file = new File("files/" + fileName);
+                if (file.exists()) {
+                    Path path = Paths.get(file.getAbsolutePath());
+                    ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName);
+                    return ResponseEntity.ok().headers(headers).contentLength(file.length())
+                            .contentType(MediaType
+                                    .parseMediaType("application/octet-stream"))
+                            .body(resource);
+                }
+                return new ResponseEntity<String>("File not found", HttpStatus.NOT_FOUND);
+
+            case 3:
+                fileName = "Ninite JDK AdoptOpenJDK x64 11 Installer.exe";
+                file = new File("files/" + fileName);
+                if (file.exists()) {
+                    Path path = Paths.get(file.getAbsolutePath());
+                    ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName);
+                    return ResponseEntity.ok().headers(headers).contentLength(file.length())
+                            .contentType(MediaType
+                                    .parseMediaType("application/octet-stream"))
+                            .body(resource);
+                }
+                return new ResponseEntity<String>("File not found", HttpStatus.NOT_FOUND);
+
+            case 4:
+                fileName = "Table Restart v.2.3.0.exe";
+                file = new File("files/" + fileName);
+                if (file.exists()) {
+                    Path path = Paths.get(file.getAbsolutePath());
+                    ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName);
+                    return ResponseEntity.ok().headers(headers).contentLength(file.length())
+                            .contentType(MediaType
+                                    .parseMediaType("application/octet-stream"))
+                            .body(resource);
+                }
+                return new ResponseEntity<String>("File not found", HttpStatus.NOT_FOUND);
+
+            case 5:
+                fileName = "tables.list";
+                file = new File("files/" + fileName);
+                if (file.exists()) {
+                    Path path = Paths.get(file.getAbsolutePath());
+                    ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName);
+                    return ResponseEntity.ok().headers(headers).contentLength(file.length())
+                            .contentType(MediaType.TEXT_PLAIN).body(resource);
+                }
+                return new ResponseEntity<String>("File not found", HttpStatus.NOT_FOUND);
+
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
---
+++ src/test/java/com/integrationToolsCollection/IntegrationToolsCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integrationToolsCollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationToolsCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
